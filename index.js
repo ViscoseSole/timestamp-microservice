34cbc248fd59e28f0f370cab6d45cb01c3fc90e1
ViscoseSole <n.hulttinen97@gmail.com>
2018-05-12 13:48:43 +0300

    Working version with some tweaks

--- index.js
+++ index.js
@@ -8,16 +8,37 @@ const
   app = express(),
   port = process.env.PORT || 3000;
 
+
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-
-
-
-app.get('/:timeString', (req, res) => {
-  //IF UNIX (NATURAL LANGUAGE FORMATTED)
-  res.send( moment.unix(req.params.timeString) );
+app.get('/:timestamp', (req, res) => {
+  const timestamp = req.params.timestamp;
+  res.send( parseTimestampJSON(timestamp) );
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+const parseTimestampJSON = (timestamp) => {
+  let result = {
+    unix: null,
+    natural: null
+  }
+
+  // @ VALID NATURAL LANGUAGE PARAMETER
+  if (moment(timestamp).isValid()) {
+    let date = moment(timestamp).format("MMMM D, YYYY");
+    result.natural = date;
+    result.unix = moment(date).unix();
+  }
+  // @ VALID UNIX PARAMETER
+  else if (moment.unix(timestamp).isValid()) {
+    result.natural = moment.unix(timestamp).format("MMMM D, YYYY");
+    result.unix = timestamp;
+  }
+
+  return result;
+}
